9d853607acf139fb9c9798a4cb8de5124e45a024
vslavav <vasla1961@gmail.com>
2018-07-13 04:14:10 +0000

    test commiting

--- MediaControl.h
+++ MediaControl.h
@@ -12,14 +12,19 @@ class MediaControl
 	//MediaCash part
 	MediaCash*            m_pMediaCashA;	// caches A and B both used for writing; when A fills up,
 	MediaCash*            m_pMediaCashB;	// B is used, while A gets dumped to disk; and vice-verse
-	unsigned int                    m_uMediaCashMaxSize;
+	unsigned int          m_uMediaCashMaxSize;
 	bool                    m_bIs_A_Active;	// currently using cache A?
 	bool              m_bIsStopThread;
 	bool              m_bIsSwitchMedia;
 	bool              m_bIsReleaseFile;
 	//CFile                   m_File;
+	bool                    m_bIsStarted;		// is Media Control thread started?
 
 	thread				_thread;
+	mutex				_mutex;
+
+	mutex				_mutex4CondVar;
+	condition_variable	_condVar;
 
 public:
 	MediaControl();
@@ -41,8 +46,16 @@ public:
 
 	void StartReleaseFile();
 	void ReleaseFile();
+
+	void SaveCashMedia2File();
+	void StartSwitchMedia();
 	
+	void CheckCondition();
 	void OpenFile();
 	void CloseFile();
+
+	mutex& GetMutex4CondVar() { return _mutex4CondVar; }
+	condition_variable& GetCondVar() { return _condVar; };
+
 };
 
